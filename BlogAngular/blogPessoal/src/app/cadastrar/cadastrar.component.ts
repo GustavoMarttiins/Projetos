9957e644c1b8ff729584b362ef4c51ea14ae6089
Martins <gustavo_marttiins@hotmail.com>
2021-04-14 11:48:16 -0300

    Alertas atualizados

--- BlogAngular/blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ BlogAngular/blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -4,6 +4,7 @@ import { UserLogin } from './../model/UserLogin';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../model/User';
 import { Router } from '@angular/router';
+import { AlertasService } from '../service/alertas.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -18,7 +19,8 @@ export class CadastrarComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private alerta: AlertasService
   ) { }
 
   ngOnInit() {
@@ -37,13 +39,13 @@ export class CadastrarComponent implements OnInit {
     this.user.tipo = this.tipoUsuario
 
     if(this.user.senha != this.confirmarSenha){
-      alert('A senhas estão incorretas.')
+      this.alerta.showAlertDanger('A senhas estão incorretas.')
     } else {
       
       this.authService.cadastrar(this.user).subscribe((resp: User) => {
         this.user = resp
         this.router.navigate(['/entrar'])
-       alert('Usuário cadastrado com sucesso!')
+       this.alerta.showAlertSuccess('Usuário cadastrado com sucesso!')
       })
     }
 
--- BlogAngular/blogPessoal/src/app/tema/tema.component.ts
+++ BlogAngular/blogPessoal/src/app/tema/tema.component.ts
@@ -4,6 +4,7 @@ import { Tema } from './../model/Tema';
 import { Router } from '@angular/router';
 import { environment } from './../../environments/environment.prod';
 import { Component, OnInit } from '@angular/core';
+import { AlertasService } from '../service/alertas.service';
 
 @Component({
   selector: 'app-tema',
@@ -17,7 +18,9 @@ export class TemaComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private alerta: AlertasService
+
   ) { }
 
   ngOnInit() {
@@ -37,7 +40,7 @@ export class TemaComponent implements OnInit {
   cadastrar(){
     this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
       this.tema = resp
-      alert('Tema cadastrado com sucesso!')
+      this.alerta.showAlertSuccess('Tema cadastrado com sucesso!')
       this.findAllTemas()
       this.tema = new Tema()
     })
